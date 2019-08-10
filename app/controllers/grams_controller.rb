73030cec445e3a8da1ef86fe296e9407af09e92b
jeff <jeffslaton@msncom>
2019-08-10 14:37:38 +0000

    added gram deail page

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -8,6 +8,13 @@ def new
     def index
     end
 
+    def show
+        @gram = Gram.find_by_id(params[:id])
+        if @gram.blank?
+            render plain: 'Not Found :(', status: :not_found
+        end
+    end
+
     def create
     @gram = current_user.grams.create(gram_params)
     if @gram.valid?
